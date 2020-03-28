8669ae0051fc6c04cfc0638b703dfc92f236c188
naveen <naveentejas1109@gmail.com>
2020-03-28 12:07:20 +0530

    Updates

---
+++ precache-manifest.6f88b6f45437c878a706502f5c4d1e3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cfcaab71ab231171c15e85f1261a23c",
+    "url": "/monster-roledex/index.html"
+  },
+  {
+    "revision": "2bd2a260756debe06e4d",
+    "url": "/monster-roledex/static/css/main.31a2582a.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-roledex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-roledex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bd2a260756debe06e4d",
+    "url": "/monster-roledex/static/js/main.5486152d.chunk.js"
+  },
+  {
+    "revision": "6a238d216f0ffca1aec2",
+    "url": "/monster-roledex/static/js/runtime-main.faf28ebe.js"
+  }
+]);
